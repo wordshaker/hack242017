b3bc15e67eb19f63455efedc2097b5a021f3d762
Jessica White <code.jessw@gmail.com>
2017-03-18 16:14:50 +0000

    Intro page

--- TheSeeker/TheSeeker/Controllers/HomeController.cs
+++ TheSeeker/TheSeeker/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace TheSeeker.Controllers
 {
     public class HomeController : Controller
     {
+        private MessagingService _messagingService;
 
         public ActionResult Index()
         {
@@ -14,6 +15,8 @@ namespace TheSeeker.Controllers
 
         public void AwaitingMembers(string name, string mobileNumber)
         {
+            // need to figure out how to assign participants to pairs.
+
             //pair{{iteration, challengeid}}
             var pairChallengeMattrix = new Dictionary<int, Dictionary<int, int>>();
             pairChallengeMattrix.Add(1, new Dictionary<int, int> {{1, 1}, {2, 3}, {3, 2}});
@@ -22,22 +25,33 @@ namespace TheSeeker.Controllers
 
             foreach (var x in pairChallengeMattrix)
             {
+                _messagingService = new MessagingService("code.jessw@gmail.com", "womenintech");
+
                 // finding out when they are free
+                //resource.AuthUrl = CronofyHelper.GetAccountIdAuthUrl();
+
                 // create calander events
+
+                _messagingService.SendScheduledMessage(new SmsMessage("07446335493", $"You have signed up to be awesome", "EX0226394"), System.DateTime.Now.AddMinutes(20));
 
                 foreach (var y in pairChallengeMattrix)
                 {
-                    var messagingService = new MessagingService("username", "password");
+                    _messagingService = new MessagingService("code.jessw@gmail.com", "womenintech");
                     // pull in times to send and set them.
                     // this needs to take a list of numbers later ie.e
                     //messagingService.SendScheduledMessages(new SmsMessageCollection("Hello!", "accountRef"), System.DateTime.Now.AddMinutes(20));
+
                     string url = "www.google.com"; //need to know how to make this into a workable textable limnk
-                    messagingService.SendScheduledMessage(new SmsMessage("07446335493", $"Hello! {url}", "accountRef"),
-                        System.DateTime.Now.AddMinutes(20));
-                    // need to insert the
+                    _messagingService.SendScheduledMessage(new SmsMessage("07446335493", $"Hello! {url}", "EX0226394"), System.DateTime.Now.AddMinutes(20));
                 }
             }
 
+        }
+
+        [HttpGet]
+        public void GetCustomerAvailability()
+        {
+            
         }
     }
 }
