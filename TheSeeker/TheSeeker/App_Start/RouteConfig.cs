571ff1f396272d720728324e86451f56ccd40f57
Jessica White <code.jessw@gmail.com>
2017-03-18 16:41:03 +0000

    add html pages

--- TheSeeker/TheSeeker/App_Start/RouteConfig.cs
+++ TheSeeker/TheSeeker/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace TheSeeker
@@ -12,11 +8,29 @@ namespace TheSeeker
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+            );
+
+            routes.MapRoute(
+                name: "Congrats",
+                url: "{action}/{id}",
+                defaults: new { action = "Congrats", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "SignUp",
+                url: "{action}/{id}",
+                defaults: new { action = "SignUp", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Confirmation",
+                url: "{action}/{id}",
+                defaults: new { action = "Confirmation", id = UrlParameter.Optional }
             );
         }
     }
