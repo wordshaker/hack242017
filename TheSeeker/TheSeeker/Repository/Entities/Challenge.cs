7331c6638726818071d8f9922fd2745fbd5664a4
Jessica White <code.jessw@gmail.com>
2017-03-19 00:59:29 +0000

    randomise challenge

--- TheSeeker/TheSeeker/Repository/Entities/Challenge.cs
+++ TheSeeker/TheSeeker/Repository/Entities/Challenge.cs
@@ -6,8 +6,7 @@ namespace TheSeeker.Repository.Entities
     {
         public string Id { get; set; }
 
-        public string Lat { get; set; }
-        public string Long { get; set; }
+        public string Map { get; set; }
         public string Description { get; set; }
     }
 }
